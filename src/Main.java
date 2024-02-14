b405fda73b63488d585165f6e3a7740f436a4014
lorahackworth <lora.g.hackworth@gmail.com>
2024-02-14 13:36:28 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Fish");
+        myList.add("Cat");
+        myList.add("Dog");
+
+        //puts items from myList into newList
+        ArrayList<String> newList = new ArrayList<>(myList);
+
+        //prints out lists, myList is preserved and newList is updated
+        removeItemFromArrayList(newList);
+        System.out.println(myList);
+        System.out.println(newList);
+    }
+
+    public static ArrayList<String> removeItemFromArrayList(ArrayList<String> a){
+        a.removeLast();
+        return a;
     }
 }
